25474ae721de46b0d500e2a1e0763dca91a5f6d6
Jake Chappelle <jake.m.chappelle@gmail.com>
2020-08-16 18:22:58 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,18 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+end
+
+def link
+  shout = "It's Dangerous To Go Alone! Take This."
+  puts shout
+end
+
+def all_phrases
+  puts(mario, toadstool, link)
 end
