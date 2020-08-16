147beeaf0752f60b8fb5e874c52f605321acf604
Jake Chappelle <jake.m.chappelle@gmail.com>
2020-08-16 18:20:55 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,9 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  shout = "It's Dangerous to Go Alone! Take This."
+  puts shout
+end
+
